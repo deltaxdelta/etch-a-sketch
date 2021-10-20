b2538967e302eda4326fe707615722fae5c5f43a
deltaxdelta <dd.deniseduke@gmail.com>
2021-10-20 14:18:44 -0400

    updates to mode menu to improve UI

--- newscript.js
+++ newscript.js
@@ -1,11 +1,3 @@
-//something like this
-//build the grid
-//add eventListener to parent of squares, stop propogation and target child node
-//function for the core eventlistener is getMode
-//getMode checks what feature is selected (option inputs)
-//runs the appropriate function for the feature (what to do with child node)
-//doesn't touch grid building or require cloning
-
 //setting up grid stuff
 
 function buildGrid(count) {
@@ -28,8 +20,8 @@ function addSquare() {
 }
 
 function wipeCanvas() {
-    let currentCanvas = document.getElementById('etchy');
-    let childSquares = currentCanvas.childNodes;
+    const currentCanvas = document.getElementById('etchy');
+    const childSquares = currentCanvas.childNodes;
     while (childSquares.length > 0) {
         currentCanvas.removeChild(childSquares[0]);
     }
@@ -41,15 +33,18 @@ function wipeCanvas() {
 const sizeSet = document.getElementById('currentSize');
 const span = document.getElementById('makeItSquare');
 
-sizeSet.addEventListener('change', getNum);
+sizeSet.addEventListener('input', getNum);
 
 function getNum() {
-    //apparently you can still type in beyond min and max values, and also the function will run with no issue, boo
-   if (sizeSet.value < 2 || sizeSet.value > 100) { 
+    //apparently you can still type in beyond min and max values, and also the function will run with no issue, this will overide
+    if (sizeSet.value > 100) {sizeSet.value = 100;}
+
+    if (sizeSet.value < 2) {sizeSet.value = 2}
+   /* if (sizeSet.value < 2 || sizeSet.value > 100) { 
         badEntry(); //show error tooltip with bad entries
-    }
+    } */
    else {
-    closeOut(); //close error message if there's a good entry put in
+   // closeOut(); //close error message if there's a good entry put in
     buildGrid(sizeSet.value);
     span.innerText = sizeSet.value; //number in input is shown in span as well for nice AxA format 
    }
@@ -128,13 +123,13 @@ function getMode(e) {
         let enteredSquare = e.target;
         
 
-        if(blackMode.checked == true) {
+        if(blackMode.checked === true) {
           
             enteredSquare.removeAttribute('style');
             enteredSquare.removeAttribute('data-bkgd');
             enteredSquare.setAttribute('style', 'background-color: black;' );}
          
-        else if (customColor.checked == true) {
+        else if (customColor.checked === true) {
         
             enteredSquare.removeAttribute('data-bkgd');
             const picker = document.getElementById('picker');
@@ -142,7 +137,7 @@ function getMode(e) {
             }
         
         
-        else if (rainbowMode.checked == true) {
+        else if (rainbowMode.checked === true) {
         
             enteredSquare.removeAttribute('style');
             enteredSquare.removeAttribute('data-bkgd');
@@ -156,46 +151,37 @@ function getMode(e) {
 
             if (enteredSquare.dataset.bkgd == 'gray1') {
                 enteredSquare.setAttribute('data-bkgd', 'gray2');
-                console.log('firing gray2');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray2') {
                 enteredSquare.setAttribute('data-bkgd', 'gray3');  
-                console.log('firing gray3'); 
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray3') {
                 enteredSquare.setAttribute('data-bkgd', 'gray4');
-                console.log('firing gray4');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray4') {
                 enteredSquare.setAttribute('data-bkgd', 'gray5');
-                console.log('firing gray5');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray5') {
                 enteredSquare.setAttribute('data-bkgd', 'gray6');
-                console.log('firing gray6');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray6') {
                 enteredSquare.setAttribute('data-bkgd', 'gray7');
-                console.log('firing gray7');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray7') {
                 enteredSquare.setAttribute('data-bkgd', 'gray8');
-                console.log('firing gray8');
             }
 
             else if (enteredSquare.dataset.bkgd == 'gray8') {
                 enteredSquare.classList.add('black');
-                console.log('firing black');
             }
 
             else {enteredSquare.setAttribute('data-bkgd', 'gray1');
-                console.log('firing gray1');
             }
         }
 
@@ -218,19 +204,30 @@ function getMode(e) {
 
 window.addEventListener('onload', buildGrid(16));
 
+
 //improving the UI
 
 //setting modes with clicks on buttons
 
-let modeArea = document.getElementById('modeArea');
-//could this just be elements by classList mode?
-
-let modeDivs = modeArea.childNodes;
-
-modeArea.addEventListener('click', colorActive);
 
 
-function colorActive(e) {
+
+let modes2 = document.getElementsByClassName('mode');
+
+let modeArray = Array.from(modes2);
+
+modeArray.forEach((leMode) => {leMode.addEventListener('click', (e) => {
+    otherClear();
+    leMode.classList.add('activeMode');
+    
+})});
+
+
+//this gets messy with so many child nodes
+
+// could this just be elements by classList mode?
+// modeArea.addEventListener('click', colorActive);
+/* function colorActive(e) {
      //remove activeMode for other buttons
     otherClear();
 
@@ -260,19 +257,21 @@ function colorActive(e) {
 
 
     e.stopPropagation(); 
-}
+} */
 
 //super stuck with removing specific class from children of modeArea so will do this for now. I either can't iterate through the pile of elements or remove method doesn't work
 //and yet this does
-function clearActive () {
+/* function clearActive () {
     document.getElementById('black').classList.remove('activeMode');
     document.getElementById('color').classList.remove('activeMode');
     document.getElementById('multi').classList.remove('activeMode');
     document.getElementById('shady').classList.remove('activeMode');
     document.getElementById('white').classList.remove('activeMode');
     
-}
+} */
 
+let modeArea = document.getElementById('modeArea');
+let modeDivs = modeArea.childNodes;
 function otherClear () {
     Array.from(modeDivs).forEach((el) => {
         if (el.id == undefined){}
@@ -280,8 +279,11 @@ function otherClear () {
     });
 }
 
+
+//I don't think these are helpful
+
 //for the divs/pseudo buttons to click radio buttons
-modeArea.addEventListener('click', setMode);
+/* modeArea.addEventListener('click', setMode);
 
 function setMode(e) {
     
@@ -307,7 +309,9 @@ function setMode(e) {
         eraseMode.checked = true;
     }
 
-}
+    else {console.log('that was not a div click')}
+
+} */
 
 //clicking label also sets active mode on psuedo button
 
@@ -321,3 +325,10 @@ function markActive(e) {
     activeRadio.parentElement.classList.add('activeMode');
 }
 
+//the color button does weird things
+colorBtn.addEventListener('click', activeBtn);
+
+function activeBtn() {
+    colorBtn.classList.add('activeMode');
+    customColor.checked = true;
+}
